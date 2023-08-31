e37aa8d5dd1d49440ba0ee387d3b4d54b2a08a4b
iammahir2020 <1810659@iub.edu.bd>
2023-08-31 22:51:07 +0600

    some more code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import Web3 from "web3";
+import {useState, useEffect} from "react";
+import SensorRanking from "./contracts/SensorRanking.json";
 import './App.css';
 
 function App() {
+  const [state,setState] = useState({web3:null, contract:null});
+  const [data,setData] = useState("nil");
+  useEffect(() => {
+    const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
+    async function fetchNetworkData() {
+      try {
+        const web3 = new Web3(provider);
+        
+        const networkID = await web3.eth.net.getId();
+        const deployedNetwork = SensorRanking.networks[networkID];
+        const contract = new web3.eth.Contract(SensorRanking?.abi,deployedNetwork?.address)
+
+        setState({web3:web3,contract:contract})
+      } catch (error) {
+        console.error("Error fetching network data:", error);
+      }
+    }
+    
+    provider && fetchNetworkData();
+  }, []);
+
+  console.log("state------------",state)
+
+  useEffect(() => {
+    async function readData() {
+      try {
+        const { contract } = state; // Use the contract directly from the state
+        if (contract) {
+          const data1 = await contract.methods.getCentralWeightPool().call();
+          console.log("data1--------- ",data1);
+          setData(data1.toString());
+        }
+      } catch (error) {
+        console.error("Error reading data:", error);
+      }
+    }
+    
+    readData();
+  }, [state]);
+
+  console.log("data-----------",data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test</p>
     </div>
   );
 }
