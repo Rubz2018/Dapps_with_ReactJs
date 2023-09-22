bc1b49163ea54fa2001f176d22f5267db4aa12c6
iammahir2020 <1810659@iub.edu.bd>
2023-09-22 23:24:29 +0600

    sensor data write and read with basic front end

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 
 function App() {
   const [state,setState] = useState({web3:null, contract:null});
-  const [data,setData] = useState("nil");
+  const [centralWeightPool,setCentralWeightPool] = useState("nil");
+  const [sensorCount,setSensorCount] = useState("nil");
+  const [sensorList,setSensorList] = useState([]);
   useEffect(() => {
     const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
     async function fetchNetworkData() {
@@ -27,28 +29,66 @@ function App() {
   }, []);
 
   useEffect(() => {
-    async function readData() {
+
+    // reading central weight pool
+    async function readCentralWeightPoolData() {
       try {
-        const { contract } = state; // Use the contract directly from the state
+        const { contract } = state;
         if (contract) {
           const data1 = await contract.methods.getCentralWeightPool().call();
-          setData(data1.toString());
+          setCentralWeightPool(data1.toString());
         }
       } catch (error) {
-        console.error("Error reading data:", error);
+        console.error("Error readCentralWeightPoolData data:", error);
       }
     }
+
+    // reading the number of sensors
+    async function readSensorCountData() {
+      try {
+        const { contract } = state;
+        if (contract) {
+          const data1 = await contract.methods.getSensorCount().call();
+          setSensorCount(data1.toString());
+        }
+      } catch (error) {
+        console.error("Error readSensorCountData data:", error);
+      }
+    }
+
+    // get sorted sensor list
+    async function readSensorListData() {
+        try {
+          const { contract } = state;
+          if (contract) {
+            const data1 = await contract.methods.getSortedSensors().call();
+            setSensorList(data1.toString());
+          }
+        } catch (error) {
+          console.error("Error getSortedSensors data:", error);
+        }
+      }
+
     
-    readData();
+    readCentralWeightPoolData();
+    readSensorCountData();
+    readSensorListData();
   }, [state]);
 
   async function writeData(){
     try{
       const {contract} = state;
-      const data = document.querySelector('#value').value;
-      await contract.methods.setter(data).send({from:"0x438539F1B1626e347B6e615e3d1C3FE1c14cEC7E"});
-      window.location.reload();
+      const sensorID = document.querySelector('#sensorID').value;
+      const moisture = parseInt(document.querySelector('#moisture').value)
+      const ph = parseInt(document.querySelector('#ph').value)
+    //   await contract.methods.addSensorReading("Sensor1", 3600, 600).send({from:"0xa5d0e56645b46a7BdBfd8F33ffAe9Ebdf9E3dd12"});
+      await contract.methods.addSensorReading(sensorID, moisture, ph).send({ from: "0xcae0B7603746bB7193d9e2eD4AaCDCbFCdd61889", gas: 200000 }); // Increase the gas limit as needed
 
+      window.location.reload();
+    // console.log('write data');
+    // console.log(typeof sensorID);
+    // console.log(typeof moisture);
+    // console.log(typeof ph);
     }catch (error) {
       console.error("Error writting data:", error);
     }
@@ -56,8 +96,18 @@ function App() {
 
   return (
     <div className="flex flex-col justify-center">
-      <p className="text-9xl font-bold">Dapp with ReactJS</p>
-      <p className="text-9xl font-bold">The data is: {data}</p>
+        <p className="text-4xl font-bold">The centralWeightPool is: {centralWeightPool}</p>
+        <p className="text-4xl font-bold">Total Sensor Count: {sensorCount}</p>
+        <p className="text-4xl font-bold">Sensor List: {sensorList}</p>
+        <div className="mb-6 flex flex-col gap-5">
+            <p className="block mb-2 text-lg font-medium text-gray-900">Sensor ID</p>
+            <input type="text" id="sensorID" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg w-[25%]"/>
+            <p className="block mb-2 text-lg font-medium text-gray-900">Moisture</p>
+            <input type="text" id="moisture" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg w-[25%]"/>
+            <p className="block mb-2 text-lg font-medium text-gray-900">Ph</p>
+            <input type="text" id="ph" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg w-[25%]"/>
+            <button onClick={writeData} className="text-white bg-blue-700 px-5 py-2 rounded-lg w-[25%]">Submit</button>
+        </div>
     </div>
   );
 }
